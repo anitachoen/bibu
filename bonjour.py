77ca603cf6e0b60c4f75163f5f9028d6b2f5ac98
Choen Anita (HES) <anita.choen@hes.adhes.hesge.ch>
2019-09-18 11:34:08 +0200

    2e sauvegarde

--- bonjour.py
+++ bonjour.py
@@ -1,2 +1,6 @@
-print ("Bonjour !");
-print ("Bienvenue aux étudiant -e-s 2019-2020!");
+print ("Bonjour !")
+print ("Bienvenue aux étudiant -e-s 2019-2020!")
+print ("Informatique de gestion")
+print ("1er semestre 2019 - 2020")
+print ("Bonjour HEG !")
+
